af2038ffa16c7a9aa7d3d7402f71934b3b5297f5
lucas.shen <lshens@hotmail.com>
2013-04-01 16:51:43 -0300

    segundo commit template

---
+++ src/web/login.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from web import formulario
+from zen import router
+
+def index(write_tmpl):
+    write_tmpl("/templates/login.html")
